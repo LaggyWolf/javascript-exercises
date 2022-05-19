96bf2b6938c2f986004bc9ee5b25c1016d9521f4
LaggyWolf <aydencheong08@gmail.com>
2022-05-19 13:54:44 +0800

    compledted test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...element) {
+    for (let i of element){
+        if (list.indexOf(i) != -1){
+            list.splice(list.indexOf(i),1);
+        };
+    };
+    return list;
 };
 
 // Do not edit below this line
