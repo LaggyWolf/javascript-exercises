abb530056be22c4ce1ed8dbea3f4a764662604f8
LaggyWolf <aydencheong08@gmail.com>
2022-05-20 19:19:29 +0800

    completed test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let new_num = 1;
+    let nums = [1,1];
+    if (number < 0){
+        return 'OOPS';
+    };
+    while (nums.length < number){
+        new_num = nums[nums.length-1] + nums[nums.length-2];
+        nums.push(new_num)
+    };
+    console.log(nums);
+    return new_num;
 };
 
 // Do not edit below this line
