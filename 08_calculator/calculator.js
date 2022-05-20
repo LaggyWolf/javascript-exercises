1179f29e88901852e72d3329d6743e362fbff4f9
LaggyWolf <aydencheong08@gmail.com>
2022-05-20 15:01:09 +0800

    completed test 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  for (let number of numbers){
+    sum += number;
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let num = 1;
+  for (let number of numbers){
+    num *= number;
+  };
+  return num;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let num = 1;
+  for (let i = 1; i <= number; i++){
+    num *= i
+  };
+  return num;
 };
 
 // Do not edit below this line
