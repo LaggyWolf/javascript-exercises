c410aba938a0d2fe64d3ada7b5c778bda2d96263
LaggyWolf <aydencheong08@gmail.com>
2022-05-20 19:02:45 +0800

    completed test 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    let new_word = '';
+    for (let letter of word){
+        if (letters.includes(letter.toLowerCase())){
+            new_word += letter;
+        };
+    };
+
+    let reverse_word = '';
+    for (let i = new_word.length - 1; i >= 0; i--){
+        reverse_word += new_word[i]; 
+    };
+
+    if (reverse_word.toLowerCase() === new_word.toLowerCase()){
+        return true;
+    };
+    return false;
 };
 
 // Do not edit below this line
