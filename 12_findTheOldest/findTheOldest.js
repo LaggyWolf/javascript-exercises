c0d6978a01108c6639581e8baa8032acec12ee3c
LaggyWolf <aydencheong08@gmail.com>
2022-05-20 20:24:38 +0800

    completed test 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest_age = 0;
+    let oldest_person;
+    const current_year = new Date().getFullYear()
+    
+    for (let person of people){
+
+        if ('yearOfDeath' in person){
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }else{
+            person.age = current_year - person.yearOfBirth;
+        };
+
+        if (person.age > oldest_age){
+            oldest_age = person.age;
+            oldest_person = person;
+        };
+    };
+
+    return oldest_person;
 
 };
 
